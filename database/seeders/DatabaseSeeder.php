b3bfcd71e99c0ce4738da713821fee84c65639b0
andirhn <tlod9906@gmail.com>
2024-03-21 17:17:11 +0800

    feat: add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        // 'name' => 'Test User',
+        // 'email' => 'test@example.com',
+        // ]);
+
+        $this->call([
+            PostSeeder::class,
+            CategorySeeder::class,
+            CategoryPostSeeder::class,
         ]);
     }
 }
