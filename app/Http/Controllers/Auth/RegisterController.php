dca30e9f345a7151061d76282d17fe5e08934771
andirhn <tlod9906@gmail.com>
2024-03-21 17:20:28 +0800

    feat: add RegisterController for handling user register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function doRegister(RegisterRequest $request)
+    {
+        $credentials = $request->validated();
+
+        $user = User::create($credentials);
+
+        Auth::login($user);
+
+        $request->session()->regenerate();
+
+        return to_route('posts.index')->with('success', 'Anda Berhasil Registrasi');
+    }
+}
