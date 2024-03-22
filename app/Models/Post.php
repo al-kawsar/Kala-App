cdd1204f00100dee69d614f34ed83dc7ca23bd18
andirhn <tlod9906@gmail.com>
2024-03-23 00:06:49 +0800

    feat: add post model

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'title',
+        'slug',
+        'body',
+        'category',
+        'author',
+        'published'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $connection = 'mysql';
+    protected $table = 'posts';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $keyType = 'integer';
+    public $timestamps = true;
+}
