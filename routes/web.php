98897a576e0cd383d791c82080f412ce94e09b36
andirhn <tlod9906@gmail.com>
2024-03-23 00:11:23 +0800

    feat: add route index page post

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::name('posts.')
+    ->controller(PostController::class)
+    ->prefix('posts')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+    });
