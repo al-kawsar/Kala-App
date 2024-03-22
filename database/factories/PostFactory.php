e8caf8c3547abf6f008b66c48492d94993b30930
andirhn <tlod9906@gmail.com>
2024-03-23 00:07:51 +0800

    feat: add post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $title = $this->faker->sentence,
+            "slug" => str()->slug($title),
+            "body" => $this->faker->paragraph(1000),
+            "category" => 'Programming,Tech',
+            "author" => $this->faker->firstName(),
+            "published" => $this->faker->dateTime(now()),
+        ];
+    }
+}
