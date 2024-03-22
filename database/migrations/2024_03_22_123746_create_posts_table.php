e285b4d9379ce85aba6dd75dacaf0bf2ba7bd319
andirhn <tlod9906@gmail.com>
2024-03-23 00:07:31 +0800

    feat: create table posts

---
+++ database/migrations/2024_03_22_123746_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable(false);
+            $table->string('slug', 255)->unique('slug')->nullable(false);
+            $table->text('body')->nullable(false);
+            $table->string('category', 50)->nullable(true);
+            $table->string('author', 50)->nullable(false);
+            $table->dateTime('published')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
