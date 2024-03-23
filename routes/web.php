4b144c8cf9dd04a4442f216ba9f71124bbb23a93
andirhn <tlod9906@gmail.com>
2024-03-23 12:01:52 +0800

    feat: add redirect route from root to '/posts'

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/posts');
 
 Route::name('posts.')
     ->controller(PostController::class)
