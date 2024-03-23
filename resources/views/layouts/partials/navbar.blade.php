482fda694247963fce0a88216138322a175ea987
andirhn <tlod9906@gmail.com>
2024-03-23 12:00:30 +0800

    feat: update navigation bar layout and add conditional elements

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,10 +1,18 @@
 <nav id="navbar" class="flex w-[80%] mx-auto justify-end">
-    <ul class="flex gap-3 p-3 ">
-        <li class="p-2 border rounded border-blue-600">
-            <a href="">Create Post</a>
-        </li>
-        <li class="p-2 rounded">
-            <a href="{{ route('posts.index') }}">Home</a>
-        </li>
+    <ul class="flex gap-3 p-3 items-center">
+        @auth
+            <li class="p-3 border rounded border-blue-600 text-blue-600">
+                <a href="">Create Post</a>
+            </li>
+        @endauth
+        @guest
+            <a href="">
+                <li class="p-3 px-4 rounded hover:bg-blue-100 hover:text-blue-600 hover:underline">Log in</li>
+            </a>
+            <a href=""
+                class="p-3  border font-bold rounded border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white hover:underline">
+                <li>Create Account</li>
+            </a>
+        @endguest
     </ul>
 </nav>
