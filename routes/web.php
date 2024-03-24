19782db03702188085a8ac380b76d80ca07b3eca
andirhn <tlod9906@gmail.com>
2024-03-24 18:55:49 +0800

    feat: add route for authenticate

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,35 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/posts');
+
+// Auth Logic
+Route::middleware('guest')->group(function () {
+    Route::controller(RegisterController::class)
+        ->prefix('register')
+        ->group(function () {
+            Route::get('/', 'index')->name('register');
+            Route::post('/', 'doRegister')->name('do-register');
+        });
+
+    Route::controller(LoginController::class)
+        ->prefix('login')
+        ->group(function () {
+            Route::get('/', 'index')->name('login');
+            Route::post('/', 'doLogin')->name('do-login');
+        });
+
+    Route::match (['get', 'post'], '/logout', LogoutController::class)
+        ->name('logout')
+        ->withoutMiddleware('guest')
+        ->middleware('auth');
+
+});
 
 Route::name('posts.')
     ->controller(PostController::class)
