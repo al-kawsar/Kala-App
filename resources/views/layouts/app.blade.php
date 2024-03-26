39d3849fafea36036224309a8956fbb473c8e8e3
andirhn <tlod9906@gmail.com>
2024-03-26 19:16:27 +0800

    feat: add body-class yield to body tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="@yield('body-class', '')">
 
     @php
         $hideNavbar = Route::is('register', 'login');
