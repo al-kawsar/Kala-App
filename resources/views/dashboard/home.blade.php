7d9ee6db3ccc64068dd911abd6ec1e1a146cced2
andirhn <tlod9906@gmail.com>
2024-03-26 17:35:09 +0800

    feat: add dashboard home view template

---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title')
+
+@section('content')
+
+    <section class="bg-gray-100 px-6">
+        <h1 class="text-3xl font-bold py-6">Dashboard</h1>
+
+        <div class="flex flex-col justify-center items-start bg-white">
+            <h1>1</h1>
+            <p>Total Artikel saya</p>
+        </div>
+    </section>
+
+@endsection
