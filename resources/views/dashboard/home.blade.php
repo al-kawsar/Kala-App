12fa78c7abad3dd9147499a18e3baad235fa8b98
Raihan Alkawsar <tlod9906@gmail.com>
2024-03-27 10:37:51 +0800

    feat: update dashboard layout and add dashboard cards

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -4,13 +4,15 @@
 
 @section('content')
 
-    <section class="bg-gray-100 px-6">
-        <h1 class="text-3xl font-bold py-6">Dashboard</h1>
+    <section class="px-6 pt-14">
+        <h1 class="text-3xl font-bold py-4">Dashboard</h1>
 
-        <div class="flex flex-col justify-center items-start bg-white">
-            <h1>1</h1>
-            <p>Total Artikel saya</p>
+        <div class="grid grid-cols-3 gap-6">
+            <x-dashboard.card icon="reader-outline" total="10" description="Total Artikel" />
+            <x-dashboard.card icon="chatbox-ellipses-outline" total="32" description="Total Komentar" />
+            <x-dashboard.card icon="time-outline" total="230" description="Total Kunjungan" />
         </div>
+
     </section>
 
 @endsection
