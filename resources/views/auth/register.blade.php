429bb671e59530df8a9851fd253d12ca08f0e23e
Raihan Alkawsar <tlod9906@gmail.com>
2024-03-27 10:40:44 +0800

    style: Improve registration form appearance

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <section class="bg-gray-50 dark:bg-gray-900 p-8">
+    <section class="bg-white dark:bg-gray-900 p-8">
         <div class="flex flex-col items-center justify-center mx-auto lg:py-0">
             <a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img class="w-12 h-12 mr-2"
@@ -13,7 +13,7 @@
                 KALA App
             </a>
             <div
-                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                class="w-full bg-white rounded-lg shadow-md dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Create account
